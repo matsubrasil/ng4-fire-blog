a23d993958e47d52ab0cc04ef608c11c308da00f
matsubrasil <matsuboy@outlook.com>
2017-06-08 00:07:39 -0300

    first component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,32 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+// component base
 import { AppComponent } from './app.component';
+
+// components
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { PostsContainerComponent } from './components/posts-container/posts-container.component';
+import { SinglePostComponent } from './components/posts-container/single-post/single-post.component';
+
+// services
+import { PostsService } from './shared/posts/posts.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    PostsContainerComponent,
+    SinglePostComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [
+    PostsService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
